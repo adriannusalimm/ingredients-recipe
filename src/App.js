fd5de2f0b7c024920e5fee009f7e786fc6857cd7
weha <wilbert26harriman@gmail.com>
2020-07-20 15:51:31 +0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="cover-container d-flex p-3 flex-column">
+      <header className="masthead mb-auto">
+        <div className="inner">
+          <h3 className="masthead-brand">Recipes</h3>
+          <nav className="nav nav-masthead justify-content-center">
+            <a className="nav-link active" href="#">Home</a>
+            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">Contact</a>
+          </nav>
+        </div>
       </header>
+
+      <main role="main" className="inner cover">
+        <h1 className="cover-heading">Search Bar</h1>
+        <p className="lead">
+          <a href="#" className="btn btn-lg btn-secondary">Search</a>
+        </p>
+      </main>
+
+      <footer className="mastfoot mt-auto">
+        <div className="inner">
+          <p>Created 2020.</p>
+        </div>
+      </footer>
     </div>
+
   );
 }
 
