d3c7e3a0a76f4e501195ffceb886a079c05f308f
weha <wilbert26harriman@gmail.com>
2020-07-24 23:59:50 +0800

    css fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Axios from 'axios'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import {v4 as uuidv4} from 'uuid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid'
 
 import './App.css';
 import Help from './pages/help'
@@ -12,70 +12,69 @@ import Alert from './components/Alert'
 
 
 const App = () => {
-  const[query, setQuery] = useState("");
-  const[recipes, setRecipes] = useState([]);
-  const[alert, setAlert] = useState("");
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
   const APP_ID = "d1730525";
   const APP_KEY = "ccc569f29e21f895cee0efa7b6db115f"
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const getData = async () => {
-      setQuery("");
-      if(query !== ""){
-          const result = await Axios.get(url);
-          if(!result.data.more){
-              return setAlert("No food with such name");
-          }
-          setRecipes(result.data.hits);
+    setQuery("");
+    if (query !== "") {
+      const result = await Axios.get(url);
+      if (!result.data.more) {
+        return setAlert("No food with such name");
+      }
+      setRecipes(result.data.hits);
 
-          setAlert("");
-      }
-      else{
-          setAlert("Please fill the form");
-      }
+      setAlert("");
+    }
+    else {
+      setAlert("Please fill the form");
+    }
   }
 
   const onSubmit = (e) => {
-      e.preventDefault();
-      getData();
+    e.preventDefault();
+    getData();
   };
 
   const onChange = (e) => {
-      setQuery(e.target.value);
+    setQuery(e.target.value);
   }
 
   return (
     <Router>
-    <div className="cover-container d-flex p-3 flex-column">
-      
-      <Header/>
-      {alert !== "" && <Alert alert={alert}/>}
+      <div className="cover-container d-flex p-3 flex-column">
+
+        <Header />
+        {alert !== "" && <Alert alert={alert} />}
 
 
-      <Route exact path="/" render={props => (
+        <Route exact path="/" render={props => (
           <React.Fragment>
             <main role="main" className="inner cover">
               <form className="search-form" onSubmit={onSubmit}>
-                <input type="search" className="search-bar form-control mx-auto" placeholder="Search for recipes..." autoComplete="off" onChange={onChange} value={query}/>
+                <input type="search" className="search-bar form-control mx-auto" placeholder="Search for recipes..." autoComplete="off" onChange={onChange} value={query} />
                 <button type="submit" className="btn btn-lg btn-success" value="search">Search</button>
               </form>
             </main>
           </React.Fragment>
-          )} />
+        )} />
 
 
-{/* load recipe */}
-    <div className = "recipes">
-        {recipes !== [] && recipes.map(recipe => 
-        <Recipe key={uuidv4()} recipe={recipe} />)}
-    </div>
+        {/* load recipe */}
+        <div className="recipes">
+          {recipes !== [] && recipes.map(recipe =>
+            <Recipe key={uuidv4()} recipe={recipe} />)}
+        </div>
 
 
-    <Route path="/help" component={Help} />
-      
-    <Footer />    
-    </div>
+        <Route path="/help" component={Help} />
 
+        <Footer />
+      </div>
     </Router>
   )
 }
